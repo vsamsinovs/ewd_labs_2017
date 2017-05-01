55e5f8ffa99c54df00ed26e45fa8435659ec549e
VAdims Samsinovs <vsamsinovs@gmail.com>
2017-05-01 16:35:49 +0100

    crud for posts

--- node_lab2/server.js
+++ node_lab2/server.js
@@ -2,6 +2,7 @@ import config from './config';
 import express from 'express';
 import contactsRouter from './api/contacts';
 import bodyParser from 'body-parser';
+import postsRouter from './api/posts';
 
 const server = express();
 
@@ -10,6 +11,8 @@ server.use(bodyParser.urlencoded());
 
 server.use('/api/contacts', contactsRouter);
 server.use(express.static('public'));
+
+server.use('/api/posts', postsRouter);
 
 server.listen(config.port, config.host, () => {
   console.info('Express listening on port', config.port);
